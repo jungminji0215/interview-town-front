0b00a3cb64f53edd15d9d550843d8886c5e8796c
jungminji <wjdalswl0215@gmail.com>
2025-03-06 17:20:55 +0900

    style : 회원가입, 로그인 페이지 스타일 수정

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -6,21 +6,24 @@ import React from "react";
 export default function LoginPage() {
   return (
     <section className="max-w-5xl mx-auto p-5 h-full flex items-center justify-center">
-      <div className="bg-white max-w-[23rem] w-full p-5 rounded-md border-2">
-        <div className="text-center pt-3 pb-5 text-2xl">로그인</div>
+      <div className="bg-white max-w-[23rem] w-full p-5">
+        <div className="text-center pt-3 pb-5 text-2xl font-title">로그인</div>
 
         {/* 로그인 Form */}
         <form className=" flex flex-col gap-3">
           <input type="email" className="border-2 rounded-md p-2"></input>
           <input type="password" className="border-2 rounded-md p-2"></input>
-          <button type="submit" className="rounded-md p-2 bg-yellow-300">
+          <button
+            type="submit"
+            className="rounded-md p-2 bg-secondary font-content"
+          >
             로그인
           </button>
         </form>
 
         <div className="flex items-center  gap-2 my-4">
           <div className="flex-1 border-t border-gray-300"></div>
-          <p className="text-gray-300 text-sm">OR</p>
+          <p className="text-gray-300 text-xs font-content">OR</p>
           <div className="flex-1 border-t border-gray-300"></div>
         </div>
 
@@ -44,8 +47,10 @@ export default function LoginPage() {
 
         {/* 회원가입 페이지 이동 버튼 */}
         <div className="text-center pt-3 py-1">
-          <Link href={ROUTES.SIGN_UP} className="text-sm text-gray-600">
-            회원가입
+          <Link href={ROUTES.SIGN_UP}>
+            <span className="text-sm text-gray-600 font-content hover:text-gray-500">
+              회원가입
+            </span>
           </Link>
         </div>
       </div>
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -3,25 +3,38 @@ import Link from "next/link";
 import React from "react";
 
 export default function SignUpPage() {
+  // TODO 로그인 form 이랑 공통
+
   return (
     <section className="max-w-5xl mx-auto p-5 h-full flex items-center justify-center">
-      <div className="bg-white max-w-[23rem] w-full p-5 rounded-md border-2">
-        <div className="text-center pt-3 pb-5 text-2xl">회원 가입</div>
+      <div className="bg-white max-w-[23rem] w-full p-5">
+        {/* TODO HOME 으로 갈 수 있는 로고 */}
+        <div className="text-center pt-3 pb-5 text-2xl font-title">
+          회원 가입
+        </div>
 
         {/* 로그인 Form */}
         <form className=" flex flex-col gap-3">
+          {/* TODO input 공통 컴포넌트 */}
           <input type="email" className="border-2 rounded-md p-2"></input>
           <input type="password" className="border-2 rounded-md p-2"></input>
-          <button type="submit" className="rounded-md p-2 bg-yellow-300">
+          <button
+            type="submit"
+            className="rounded-md p-2 bg-secondary font-content"
+          >
             회원 가입
           </button>
         </form>
 
         {/* 로그인 페이지 이동 버튼 */}
         <div className="flex justify-center items-center gap-2 py-1 pt-5">
-          <p className="text-xs text-gray-600">이미 아이디가 있으신가요?</p>
-          <Link href={ROUTES.LOGIN} className="text-xs text-gray-600">
-            로그인
+          <p className="text-sm text-gray-600 font-content">
+            이미 아이디가 있으신가요?
+          </p>
+          <Link href={ROUTES.LOGIN}>
+            <span className="text-sm text-gray-600 font-content hover:text-gray-500">
+              로그인
+            </span>
           </Link>
         </div>
       </div>
