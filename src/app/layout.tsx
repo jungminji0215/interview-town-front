91544b8c943f59f3ba8de22b761269726025c74b
jungminji <wjdalswl0215@gmail.com>
2025-05-04 03:12:28 +0900

    [feat] 메타데이터 설정 home -> layout 으로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,29 @@ import React, { ReactNode } from 'react';
 import Header from '@/components/Header';
 import Providers from '@/providers/QueryProvider';
 
+export const metadata: Metadata = {
+  title: '면접 타운',
+  description: '개발자 면접을 함께 연습하는 공간',
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    type: 'website',
+    title: '면접 타운',
+    description: '면접 준비가 필요할때, 면접 타운',
+    url: 'https://www.interview-town.com',
+    siteName: '면접 타운',
+    images: [
+      {
+        url: '/thumbnail.png',
+        width: 1200,
+        height: 630,
+        alt: 'thumbnail image',
+      },
+    ],
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
