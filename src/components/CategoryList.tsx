388465f9ba05c6debcc718f3de5439d673569f36
jungminji <wjdalswl0215@gmail.com>
2025-05-09 04:00:43 +0900

    [refactor](category) : all 카테고리 분리

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,16 +1,14 @@
 import { getCategories } from '@/api/categories';
 import CategoryItem from '@/components/CategoryItem';
-import Link from 'next/link';
 
 export default async function CategoryList() {
   const categories = await getCategories();
-  const allCategories = [{ id: 0, name: 'all' }, ...categories];
 
   return (
     <nav aria-label="카테고리 네비게이션" className="py-4">
       <ul className="flex gap-3 overflow-x-auto">
-        {/* 스크롤 가능 */}
-        {allCategories.map((category) => (
+        <CategoryItem key={0} category={{ id: 0, name: 'all' }} />
+        {categories.map((category) => (
           <CategoryItem key={category.id} category={category} />
         ))}
       </ul>
